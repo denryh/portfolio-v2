8350bdb82048ed6ca3525eea786d8b1667fae3dc
dgthanhdat <85379455+denryh@users.noreply.github.com>
2023-02-26 19:30:37 +0700

    Update config.ts

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,5 +1,5 @@
 export const TRANSITION_TIME = 500;
 export const RESUME_URL_EN =
-  "https://drive.google.com/file/d/1ZZ6kLJUcfSeDcuq6alnCkQ6lOx1duJX_/view";
+  "https://drive.google.com/file/d/1FfzniGRqqdNBdg2xaZda1liW181Q-CW0/view?usp=share_link";
 export const RESUME_URL_VN =
   "https://drive.google.com/file/d/1-T-OVMVTW1GDoTkA_Do_xdGDkByiJjkK/view";
